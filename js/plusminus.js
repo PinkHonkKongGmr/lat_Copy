28f3bf9b228ac383d94d221dfc5d5a29f615341f
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-11-15 02:32:11 +0300

    Add files via upload

--- js/plusminus.js
+++ js/plusminus.js
@@ -1,29 +1,37 @@
-
+var togglers = document.querySelectorAll('.toggler');
+var infos = document.querySelectorAll('.info');
 var pluses = document.querySelectorAll('.plus');
+var minuses = document.querySelectorAll('.minus');
+var questions = document.querySelectorAll('.question');
 
+for (let toggle = 0; toggle < togglers.length; toggle++) {
 
+  let thatTogglers;
+  let thatInfo;
+  let thatPlus;
+  let thatMinus;
 
+  thatTogglers = togglers[toggle];
+  thatInfo = infos[toggle];
+  thatPlus = pluses[toggle];
+  thatMinus = minuses[toggle];
+  $(thatTogglers).click(function() {
+    $(thatTogglers).toggleClass('itsgreen');
+    $(thatPlus).slideToggle(0);
+    $(thatMinus).slideToggle(0);
+    $(thatInfo).slideToggle();
 
-
-for (let plus=0;plus<pluses.length;plus++) {
-
-  let that;
-  let onplus=true;
-
-
-  that=pluses[plus];
-  $(that).click(function() {
-  if (onplus) {
-    $(that).hide()
-    onplus=false;
-    return;
-  }
-
-  if (onplus==false) {
-    $(that).show()
-    onplus=true;
-    return;
-  }
   });
+}
 
+
+for (let toggle = 0; toggle < togglers.length; toggle++) {
+
+  togglers[toggle].onmouseover = function() {
+    questions[toggle].classList.toggle('fontGreen');
+  }
+
+  togglers[toggle].onmouseout = function() {
+    questions[toggle].classList.toggle('fontGreen');
+  }
 }
