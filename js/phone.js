efa056f49d9ddc867878b3ebe25c97375552703e
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-11-22 22:57:58 +0300

    Add files via upload

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,14 @@
 $(function(){
   $(".phone").mask("8(999) 999-99-99");
 });
+
+var circleCall =document.querySelector('.roundCall')
+circleCall.onmouseout = function (e) {
+
+
+circleCall.classList.toggle('greenCall');
+
+
+
+
+}
