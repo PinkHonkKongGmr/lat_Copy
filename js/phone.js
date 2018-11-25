72d4881bd38a5468068cb0c71a093b8da16f2847
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-11-25 22:24:24 +0300

    Add files via upload

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,6 @@
 var colorblink=false;
 var circleCall =document.querySelector('.roundCall');
+var blink;
 
 $(function(){
   $(".phone").mask("8(999) 999-99-99");
@@ -9,29 +10,45 @@ $(function(){
 function red() {
 
   $(circleCall).animate({
-  		backgroundColor: "red"
+  		backgroundColor: "#ff6534"
   	}, 2500 );
 
 }
 
 function changeColor() {
 
+if (blink) {
+
   $(circleCall).animate({
-  		backgroundColor: "#abcdef"
-  	}, 2500 );
+      backgroundColor: "#00aa00"
+    }, 2500 );
 
     setTimeout(red,2500)
 
 }
 
+}
+
 function girlyanda() {
+
+  $(circleCall).animate({
+      backgroundColor: "#f48634"
+    }, 5000 );
     setInterval(changeColor,5000)
 }
 
 
 circleCall.onmouseover = function (e) {
 
+  blink=true;
+
   colorblink=true;
 
 girlyanda();
+}
+
+circleCall.onmouseout = function (e) {
+
+  blink=false;
+
 }
