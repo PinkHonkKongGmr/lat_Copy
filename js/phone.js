98db80bd2ae691acd8c81c37f5faed3fb7beb9de
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-11-25 23:58:58 +0300

    Add files via upload

--- js/phone.js
+++ js/phone.js
@@ -1,54 +1,42 @@
-var colorblink=false;
-var circleCall =document.querySelector('.roundCall');
-var blink;
+// var blink;
+var circleCall = document.querySelector('.roundCall');
 
-$(function(){
+$(function() {
   $(".phone").mask("8(999) 999-99-99");
 });
-
 
 function red() {
 
   $(circleCall).animate({
-  		backgroundColor: "#ff6534"
-  	}, 2500 );
-
+    backgroundColor: "#ff6534"
+  }, 2500);
 }
 
 function changeColor() {
 
-if (blink) {
+  if (blink) {
 
-  $(circleCall).animate({
+    $(circleCall).animate({
       backgroundColor: "#00aa00"
-    }, 2500 );
-
-    setTimeout(red,2500)
-
-}
-
+    }, 2500);
+    setTimeout(red, 2500)
+  }
 }
 
 function girlyanda() {
 
   $(circleCall).animate({
-      backgroundColor: "#f48634"
-    }, 5000 );
-    setInterval(changeColor,5000)
+    backgroundColor: "#f48634"
+  }, 5000);
+  setInterval(changeColor, 5000)
 }
 
 
-circleCall.onmouseover = function (e) {
-
-  blink=true;
-
-  colorblink=true;
-
-girlyanda();
+circleCall.onmouseover = function(e) {
+  // blink = true;
+  girlyanda();
 }
 
-circleCall.onmouseout = function (e) {
-
-  blink=false;
-
-}
+// circleCall.onmouseout = function(e) {
+//   blink = false;
+// }
